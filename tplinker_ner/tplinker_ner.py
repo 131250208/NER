c12d5ae0ea82d4d3859c795338f7b30ea9c4e767
wycheng <wychengpublic@163.com>
2020-08-08 18:05:16 +0800

    add GHM to loss func

--- tplinker_ner/tplinker_ner.py
+++ tplinker_ner/tplinker_ner.py
@@ -205,10 +205,54 @@ class Metrics:
     def __init__(self, handshaking_tagger):
         super().__init__()
         self.handshaking_tagger = handshaking_tagger
-    
+        self.last_weights = None
+        
+    def GHM(self, gradient, bins = 10, beta = 0.9):
+        '''
+        gradient_norm: gradient_norms of all examples in this batch; (batch_size, shaking_seq_len)
+        '''
+        avg = torch.mean(gradient)
+        std = torch.std(gradient) + 1e-12
+        gradient_norm = torch.sigmoid((gradient - avg) / std) # normalization and pass through sigmoid to 0 ~ 1.
+        
+        min_, max_ = torch.min(gradient_norm), torch.max(gradient_norm)
+        gradient_norm = (gradient_norm - min_) / (max_ - min_)
+
+        example_sum = torch.flatten(gradient_norm).size()[0] # N
+
+        # calculate weights
+            
+        current_weights = torch.zeros(bins).to(gradient.device)
+        hits_vec = torch.zeros(bins).to(gradient.device)
+        count_hits = 0 # coungradient_normof hits
+        for i in range(bins):
+            bar = float((i + 1) / bins)
+            hits = torch.sum((gradient_norm <= bar)) - count_hits
+            count_hits += hits
+            hits_vec[i] = hits.item()
+            current_weights[i] = example_sum / bins / (hits.item() + example_sum / bins )
+        # EMA: exponential moving averaging
+        print()
+        print("hits_vec: {}".format(hits_vec))
+        print("current_weights: {}".format(current_weights))
+        if self.last_weights is None:
+            self.last_weights = torch.ones(bins).to(gradient.device) # init by ones
+        current_weights = self.last_weights * beta + (1 - beta) * current_weights
+        self.last_weights = current_weights
+        print("ema current_weights: {}".format(current_weights))
+        
+        # weights4examples: pick weights for all examples
+        weight_pk_idx = ((gradient_norm - 0.001) / (1 / bins)).long()[:, :, None] # - 0.001: ensure elements in gradient_norm < 1.
+        weights_rp = current_weights[None, None, :].repeat(gradient_norm.size()[0], gradient_norm.size()[1], 1)
+        weights4examples = torch.gather(weights_rp, -1, weight_pk_idx).squeeze(-1)
+        weights4examples /= torch.sum(weights4examples)
+        return weights4examples * gradient # return weighted gradients
+
     # loss func
     def _multilabel_categorical_crossentropy(self, y_pred, y_true):
         """
+        y_pred: (batch_size, shaking_seq_len, type_size)
+        y_true: (batch_size, shaking_seq_len, type_size)
         y_true and y_pred have the same shape，elements in y_true are either 0 or 1，
              1 tags positive classes，0 tags negtive classes(means tok-pair does not have this type of link).
         """
@@ -220,7 +264,7 @@ class Metrics:
         y_pred_pos = torch.cat([y_pred_pos, zeros], dim = -1)
         neg_loss = torch.logsumexp(y_pred_neg, dim = -1) 
         pos_loss = torch.logsumexp(y_pred_pos, dim = -1) 
-        return (neg_loss + pos_loss).mean()
+        return (self.GHM(neg_loss + pos_loss, bins = 100)).sum() 
     
     def loss_func(self, y_pred, y_true):
         return self._multilabel_categorical_crossentropy(y_pred, y_true)
