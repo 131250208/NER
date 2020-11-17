69b8f8b7914db41143fa9e227baee487bb47488a
wyc <wychengpublic@163.com>
2020-11-17 10:02:30 -0500

    version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/131250208/yucheng_ner'
 EMAIL = 'wangyucheng@iie.ac.cn'
 AUTHOR = 'Andy Wong'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = 0.1
+VERSION = 0.2
 
 # What packages are required for this module to be executed?
 REQUIRED = [
